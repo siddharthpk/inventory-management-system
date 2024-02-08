a08fa6db6cdd60be572731cb4707b83d8b00226b
Siddharth Pathak <siddharthpathak415@gmail.com>
2024-02-07 22:03:38 -0800

    Intial commit

---
+++ inventory-management/src/test/java/com/spathak/inventorymanagement/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spathak.inventorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
